e0b8a105c61fdadce84c0390330e06b393faed3d
Eulga <kimyeoun10@gmail.com>
2023-09-18 22:26:06 +0900

    coding test example

---
+++ src/programmers/동적계획법_DP/정수삼각형/Main.java
@@ -0,0 +1,15 @@
+package programmers.동적계획법_DP.정수삼각형;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}));
+    }
+}
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        return answer;
+    }
+}
